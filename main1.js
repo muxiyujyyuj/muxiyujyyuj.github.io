22ac274289877d255cd2f95ed4226f5e3a852da6
muxiyuj <127747341+muxiyujyyuj@users.noreply.github.com>
2023-03-20 21:35:05 +0800

    Update main1.js

--- main1.js
+++ main1.js
@@ -1,5 +1,5 @@
 let arr = [
-    'https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+    'https://s3.bmp.ovh/imgs/2023/03/20/be314a3652a7d576.jpg',
     'https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
     'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80',
     'https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
